17dd370026aaf200802725c332446b82abfbb1ab
noveberme <evgeniyagagra@gmail.com>
2025-07-17 00:23:42 +0300

    add new class

---
+++ src/main/java/main/java/entities/TicketQuota.java
@@ -0,0 +1,45 @@
+package main.java.entities;
+
+import main.java.enums.PerfomanceName;
+
+public class TicketQuota {
+    private final PerfomanceName perfomanceName;
+    private final int maxTickets;
+    private int soldTickets;
+
+    public TicketQuota(PerfomanceName perfomanceName, int maxTickets) {
+        this.perfomanceName = perfomanceName;
+        this.maxTickets = maxTickets;
+        this.soldTickets = 0;
+    }
+
+    public PerfomanceName getPerfomanceName() {
+        return perfomanceName;
+    }
+
+    public int getMaxTickets() {
+        return maxTickets;
+    }
+
+    public int getSoldTickets() {
+        return soldTickets;
+    }
+
+    public boolean canSellTicket() {
+        return soldTickets < maxTickets;
+    }
+
+    public void sellTicket() {
+        if (canSellTicket()) {
+            soldTickets++;
+        } else {
+            throw new IllegalStateException("Достигнуто максимальное количество билетов для " + perfomanceName);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: продано %d из %d билетов",
+                perfomanceName, soldTickets, maxTickets);
+    }
+}
